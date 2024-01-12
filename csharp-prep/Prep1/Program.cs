1886f277fe16a035c3508c84977de3e0566b8b37
RetroRaven9 <122410075+RetroRaven9@users.noreply.github.com>
2024-01-12 15:46:47 -0700

    nameprogram

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        //first ask them for their name whith this line
+        Console.WriteLine("What is your firstname?");
+        //this is the line that will they will inpute
+        string firstname = Console.ReadLine();
+        //reapeate the other lines for the last name
+        Console.WriteLine("What is your lastname?");
+        string lastname = Console.ReadLine();
+        
+        //this line will be displayed on the console with their lastname,firstname,lastname
+        Console.WriteLine("your name is " + lastname + "," + firstname + "," + lastname);
+
+
+    
+
+    
     }
+
+
 }
