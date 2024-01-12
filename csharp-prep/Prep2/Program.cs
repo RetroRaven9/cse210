03d660c840e70ed93b76fab6afc65cd8a0485eb2
RetroRaven9 <122410075+RetroRaven9@users.noreply.github.com>
2024-01-12 16:16:17 -0700

    lettergrade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your class score?");
+        string score = Console.ReadLine();
+        
+        int grade = int.Parse(score);
+        
+        if(grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+
+        else if(grade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+
+        else if(grade >= 70)
+        {
+            Console.WriteLine("C");
+        }   
+
+        else if(grade < 60)
+        {
+            Console.WriteLine("D");
+        }
+    
+        
+    
+    
+    
     }
+
 }
