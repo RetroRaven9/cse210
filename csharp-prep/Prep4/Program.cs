7a6bf462c5d17a5bd01d391c6d82fb9a7d01cb23
Tylar Hill <tylarbh@byui.edu>
2024-01-19 14:23:19 -0700

    the progam lists numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> listWithNumbers = new List<int>();
+
+        int numberInList;
+
+        Console.WriteLine("Enter a list of Number, type 0 when finished.");
+        
+
+        do
+        {
+            
+            Console.Write("Enter a number");
+            string input = Console.ReadLine();
+
+            if(int.TryParse(input, out numberInList))
+            {
+                if (numberInList != 0)
+                {
+                    listWithNumbers.Add(numberInList);
+                }   
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+
+        }while (numberInList != 0);
+
+        
+
+
+        List<int> sortedlist = listWithNumbers.OrderBy(x => x).ToList();
+        Console.WriteLine("Sorted list is:");
+        foreach (int number in sortedlist)
+        {
+            Console.WriteLine(number);
+        }
+
+
     }
+    
 }
