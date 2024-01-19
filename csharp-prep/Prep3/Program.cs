dcdb8cbc12b767fef6d667aa68b1060389949415
Tylar Hill <tylarbh@byui.edu>
2024-01-19 13:25:41 -0700

    Game to quess random number, got stuck. got some help with syntax and other stuff from chatgpt

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,43 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Console.WriteLine("Welcome to the random number game please guess a number from 1-100 ");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int userGuess;
+
+        do
+        {   
+            Console.WriteLine("What is your guess?");
+            string userGuessString = Console.ReadLine();
+        
+            if(int.TryParse(userGuessString, out userGuess))
+            {
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You Won!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid guess");
+            }
+            
+            
+
+        }while (userGuess != magicNumber);
     }
 }
