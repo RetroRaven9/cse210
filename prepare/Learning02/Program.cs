bc2443bea2df4c4dbaba47e9a5c40e9cd3e35e7f
Tylar Hill <tylarbh@byui.edu>
2024-01-24 18:37:25 -0700

    Job resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company =  "Microsoft";
+        job1._startYear = 1985;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Game Developer";
+        job2._company =  "Sony Entertainment";
+        job2._startYear = 2002;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Tylar Hill";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
+
+       
     }
 }
