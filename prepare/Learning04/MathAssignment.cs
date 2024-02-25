f845babbe4dedc121f38573b8d5e4da6a4b910d9
Tylar Hill <tylarbh@byui.edu>
2024-02-24 19:45:15 -0700

    idk

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,18 @@
+public class MathAssignment : Assignment
+{
+   private string _textbookSelection;
+   private string _problems;
+
+   public MathAssignment(string studentName, string topic, string textbookSelection, string problems) 
+   : base(studentName, topic)
+   {
+      _textbookSelection = textbookSelection;
+      _problems = problems;
+   }
+
+   public string GetHomeworkList()
+   {
+      return $"section {_textbookSelection} problems {_problems}";
+   }
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Net.Http.Headers;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WrittingAssignment a3 = new WrittingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation()); 
+
     }
 }
